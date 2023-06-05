995dacc1f562962b7a5789e24c9015420360c3a5
Louis Klein <louisklein71@gmail.com>
2023-06-05 08:58:35 -0700

    dev

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 import Header from "./Header";
 import AlcoholList from "./AlcoholList"
 import DrinkContainer from "./DrinkContainer"
 
 function App() {
+
+
   return (
     <div className="App">
       <Header />
